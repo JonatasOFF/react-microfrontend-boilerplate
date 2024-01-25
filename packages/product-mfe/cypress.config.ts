585188fbeba3e9d459b3a0be15d32941734f15ad
JonatasOFF <jonatasoliveiraferreira158@gmail.com>
2024-01-24 23:56:55 -0300

    change all to package

--- packages/product-mfe/cypress.config.ts
+++ packages/product-mfe/cypress.config.ts
@@ -4,11 +4,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       return {
+        ...config,
         baseUrl: "http://localhost:3001",
         viewportWidth: 1366,
         viewportHeight: 768,
         env: {
-          HOST_NAME_API: "dev-core.iclubs.io",
+          HOST_NAME_API: "",
         },
       };
     },
